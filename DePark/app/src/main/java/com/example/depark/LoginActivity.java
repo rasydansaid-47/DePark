8880489aadb3e7b19eebfef472606bea504bf556
DESKTOP-FDT5K1G\user <rasydansaidazmirais@gmail.com>
2020-08-01 15:18:53 +0800

    Login & Register with Firebase Authentication Database

--- DePark/app/src/main/java/com/example/depark/LoginActivity.java
+++ DePark/app/src/main/java/com/example/depark/LoginActivity.java
@@ -1,45 +1,57 @@
 package com.example.depark;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText e1,e2;
-    TextView e3,e4;
-    Button b1;
-    FirebaseAuth firebaseAuth;
-    int counter = 5;
+    private EditText e1,e2;
+    private TextView e3,e4;
+    private Button b1;
+    private FirebaseAuth firebaseAuth;
+    private int counter = 5;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        b1 = (Button)findViewById(R.id.btnLogin);
         e1 = (EditText)findViewById(R.id.txtUsername);
         e2 = (EditText)findViewById(R.id.txtPwd);
         e3 = (TextView)findViewById(R.id.txtInfo);
         e4 = (TextView)findViewById(R.id.txtRegister);
+        b1 = (Button)findViewById(R.id.btnLogin);
 
         e3.setText("No of attempts remaining: 5");
 
-        b1.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
+        if(user != null){
+            finish();
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }
+
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = e1.getText().toString();
-                String password = e2.getText().toString();
-                validate(username,password);
+                validate(e1.getText().toString(),e2.getText().toString());
             }
         });
 
@@ -53,19 +65,27 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void validate(String username, String userpasswrd){
-        if((username.equals("Admin")) && (userpasswrd.equals("1234"))){
-            Toast.makeText(getApplicationContext(),"Successfully Login", Toast.LENGTH_SHORT).show();
-            Intent intent2 = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent2);
-        }
-        else{
-            counter--;
+        progressDialog.setMessage("Hello There!");
+        progressDialog.show();
 
-            e3.setText("No of attempts remaining: " + String.valueOf(counter));
-
-            if(counter == 0){
-                b1.setEnabled(false);
+        firebaseAuth.signInWithEmailAndPassword(username, userpasswrd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+                    counter--;
+                    e3.setText("No of Attempts Remaining: " + counter);
+                    progressDialog.dismiss();
+                    if(counter == 0){
+                        b1.setEnabled(false);
+                    }
+                }
             }
-        }
+        });
     }
 }
--- DePark/app/src/main/java/com/example/depark/RegisterActivity.java
+++ DePark/app/src/main/java/com/example/depark/RegisterActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.text.method.LinkMovementMethod;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,49 +19,33 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity{
 
-    EditText e1, e2, e3, e4;
-    TextView e5;
-    Button b1;
+    private EditText e1, e2, e3, e4;
+    private TextView e5;
+    private Button b1;
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        e1 = (EditText) findViewById(R.id.txtName);
-        e2 = (EditText) findViewById(R.id.txtEmail);
-        e3 = (EditText) findViewById(R.id.txtPwd);
-        e4 = (EditText) findViewById(R.id.txtCPwd);
-        e5 = (TextView) findViewById(R.id.txtLogin);
-        b1 = (Button) findViewById(R.id.btnLogin);
+        setupUIView();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        e5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-
         b1.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View v){
-
                 if(validate()){
-                    String s1 = e2.getText().toString();
-                    String s2 = e3.getText().toString();
+                    String s1 = e2.getText().toString().trim();
+                    String s2 = e3.getText().toString().trim();
 
-                    firebaseAuth .createUserWithEmailAndPassword(s1,s2).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    firebaseAuth.createUserWithEmailAndPassword(s1,s2).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
                                 Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_SHORT).show();
-                                Intent intent2 = new Intent(RegisterActivity.this, LoginActivity.class);
-                                startActivity(intent2);
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
                             }
                             else {
                                 Toast.makeText(getApplicationContext(),"Registered Successfully", Toast.LENGTH_SHORT).show();
@@ -71,24 +55,43 @@ public class RegisterActivity extends AppCompatActivity{
                 }
             }
         });
+
+        e5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    private boolean validate(){
+    private void setupUIView() {
+        e1 = (EditText) findViewById(R.id.txtName);
+        e2 = (EditText) findViewById(R.id.txtEmail);
+        e3 = (EditText) findViewById(R.id.txtPwd);
+        e4 = (EditText) findViewById(R.id.txtCPwd);
+        e5 = (TextView) findViewById(R.id.txtLogin);
+        b1 = (Button) findViewById(R.id.btnRegister);
+    }
+
+    private Boolean validate(){
         boolean result = false;
+
         String s1 = e1.getText().toString();
         String s2 = e2.getText().toString();
         String s3 = e3.getText().toString();
         String s4 = e4.getText().toString();
 
-        if(s1.isEmpty()||s2.isEmpty()||s3.isEmpty()||s4.isEmpty()){
+        if(s1.isEmpty() && s2.isEmpty() && s3.isEmpty() && s4.isEmpty()){
             Toast.makeText(getApplicationContext(), "Fields are empty", Toast.LENGTH_SHORT).show();
         }
-        else if(s3.equals(s4) == false){
+        else if(s3.equals(s4) != true){
             Toast.makeText(getApplicationContext(), "Password and Confirm Passsword are not same", Toast.LENGTH_SHORT).show();
         }
         else {
             result = true;
         }
+
         return result;
     }
 }
