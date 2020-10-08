8f0189cd540130f729ec5e31839ccb249047e142
Akimiz <ahmadakimi@gmail.com>
2020-10-08 23:36:22 +0800

    last update ui kot

--- DePark/app/src/main/java/com/example/depark/AdminProfileFragment.java
+++ DePark/app/src/main/java/com/example/depark/AdminProfileFragment.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -39,7 +40,7 @@ public class AdminProfileFragment extends AppCompatActivity {
     String name, address, phone, image;
     ImageView img;
     TextView t1, t2;
-    ImageButton b1, b2;
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- DePark/app/src/main/java/com/example/depark/ReceiptListFragment.java
+++ DePark/app/src/main/java/com/example/depark/ReceiptListFragment.java
@@ -32,7 +32,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
-public class ReceiptListFragement extends AppCompatActivity {
+public class ReceiptListFragment extends AppCompatActivity {
 
     private static final String TAG = "ValetListFragment";
     private static final String REQUIRED = "Required";
@@ -55,7 +55,7 @@ public class ReceiptListFragement extends AppCompatActivity {
 
         l1 = findViewById(R.id.listview);
         e1 = findViewById(R.id.etLot);
-        e2 = findViewById(R.id.etStatus);
+        e2 = findViewById(R.id.etdate);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser =  firebaseAuth.getCurrentUser();
@@ -70,26 +70,32 @@ public class ReceiptListFragement extends AppCompatActivity {
             @Override
             public void onDataChange (@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot dataSnapshot: snapshot.getChildren()) {
-                    final String ref = dataSnapshot.child("ref").getValue().toString();
-                    final String author = dataSnapshot.child("author").getValue().toString();
-                    final String lot = dataSnapshot.child("lot").getValue().toString();
-                    final String start = dataSnapshot.child("start").getValue().toString();
-                    final String end = dataSnapshot.child("end").getValue().toString();
-                    final String state = dataSnapshot.child("paymentState").getValue().toString();
-                    final String total = dataSnapshot.child("total").getValue().toString();
-                    arrayAdapter.add("Parking Lot: " + lot + "Date: " + start);
-                    l1.setAdapter(arrayAdapter);
-                    l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                        @Override
-                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(ReceiptListFragement.this);
-                            builder.setTitle("Booking Date " + start + " to " + end);
-                            builder.setMessage("No.ref: " + ref + "\n" + "Name: " + author + "\n" + "Parking Lot: " + lot + "\n" +
-                                    "Payment State: " + state + "\n" + "Total Price: RM" + total);
-                            AlertDialog alert1 = builder.create();
-                            alert1.show();
-                        }
-                    });
+                    if (dataSnapshot != null) {
+                        final String ref = dataSnapshot.child("ref").getValue().toString();
+                        final String author = dataSnapshot.child("author").getValue().toString();
+                        final String lot = dataSnapshot.child("lot").getValue().toString();
+                        final String start = dataSnapshot.child("start").getValue().toString();
+                        final String end = dataSnapshot.child("end").getValue().toString();
+                        final String state = dataSnapshot.child("paymentState").getValue().toString();
+                        final String total = dataSnapshot.child("total").getValue().toString();
+                        arrayAdapter.add("Parking Lot: " + lot + "Date: " + start);
+                        l1.setAdapter(arrayAdapter);
+                        l1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(ReceiptListFragment.this);
+                                builder.setTitle("Booking Date " + start + " to " + end);
+                                builder.setMessage("No.ref: " + ref + "\n" + "Name: " + author + "\n" + "Parking Lot: " + lot + "\n" +
+                                        "Payment State: " + state + "\n" + "Total Price: RM" + total);
+                                AlertDialog alert1 = builder.create();
+                                alert1.show();
+                            }
+                        });
+
+                    }else {
+                        Toast.makeText(getApplicationContext(), "No data yet", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                 }
             }
 
@@ -128,6 +134,9 @@ public class ReceiptListFragement extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                arrayAdapter.notifyDataSetChanged();
+                l1.invalidateViews();
+                l1.refreshDrawableState();
 
             }
         });
